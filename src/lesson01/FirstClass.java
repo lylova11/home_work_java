b1a34a2e36924cbc71b998138cd99ca4df408294
lylova11 <nemova94@mail.ru>
2022-01-15 21:46:38 +0300

    Прошла лекция

---
+++ src/lesson01/FirstClass.java
@@ -0,0 +1,10 @@
+package lesson01;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.print("Hello world!");
+        System.out.println("Hello world!");
+        System.out.print("Hello world!");
+    }
+}
