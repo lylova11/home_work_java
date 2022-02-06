5930f5eee8f967693d9178ad0fc23a7f3a69b902
lylova11 <nemova94@mail.ru>
2022-02-06 21:46:02 +0300

    home_work_07

---
+++ src/lesson07andHomeWork/GameField.java
@@ -0,0 +1,15 @@
+package lesson07andHomeWork;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameField extends JPanel {
+
+    GameField() {
+        setBackground(new Color(194, 124, 194));
+
+    }
+
+
+
+}
---
+++ src/lesson07andHomeWork/Runner.java
@@ -0,0 +1,9 @@
+package lesson07andHomeWork;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        new GameWindow();
+
+    }
+}
